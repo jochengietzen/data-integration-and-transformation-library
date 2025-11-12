4d7804940ec55d5d08cf5962b488c72d5fa94c91
Jochen Gietzen <jochen.gietzen@gmail.com>
2025-11-12 19:20:18 +0000

    dev commit - type var for Table expectation and path! Real improvement

--- src/ditl/engines/base.py
+++ src/ditl/engines/base.py
@@ -24,3 +24,20 @@ class Engine(BaseModel):
                 cls.engine_identifier
             ]
         ] = data_type
+
+    @abstractmethod
+    def read(cls,  read_type: EngineReadType, *args, **kwargs) -> DataFrameWrapper:
+        pass
+
+    # Aktuelle Idee!
+    # Wir nutzen doch eine Registrierung für read und write, damit die Nachimplementierung so einfach wie möglich ist.
+    # Idee ist, per String key eine methode einzuhängen, die dann aus der Table heraus aufgerufen wird.
+    # Read und Write werden dann auf Engine Base Ebene implementiert und geben eine eindeutige Fehlermeldung, wenn diese
+    # nicht existiert. Wir können dann beliebig vorimplementieren. Eventuell fliegt dann der EngineReadType wieder raus
+    # oder wird zu einem ReadWriteType aber durch string key unwahrscheinlich!
+    # Falls diese Änderungen verworfen werden, unbedingt beim Table den Generic für TablePathType und TableExpectationType rausnehmen,
+    # das kann durch die TypeVar von pydantic korrekt aufgelöst werden. Also ohne Generic in der Table einfach:
+    # expectations: list[TableExpectationType] = Field(default_factory=list)
+
+
+EngineType = TypeVar("EngineType", bound=Engine)
